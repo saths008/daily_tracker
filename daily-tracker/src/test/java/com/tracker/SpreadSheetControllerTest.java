9a76392e1745791413dd7d285ece27f007e0940c
Saath Satheeshkumar <saathsatheesh@gmail.com>
2023-07-16 14:57:08 +0100

    feat: Add testing skeleton

---
+++ daily-tracker/src/test/java/com/tracker/SpreadSheetControllerTest.java
@@ -0,0 +1,13 @@
+package com.tracker;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpreadSheetControllerTest {
+
+    @Test
+    public void testSum() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+
+}
